9e5959de493a19382cc40b2bb3e3914ccef1e67e
BuildTools <williamcaetano14@gmail.com>
2021-07-28 23:20:51 -0300

    :sparkles: Added logic to create user on supabase

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,20 +1,61 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Box,
   FormControl,
+  FormLabel,
   FormErrorMessage,
   Heading,
   Input,
   Stack,
   Button,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import { FlexArea } from "../../components/FlexArea";
 import { Header } from "../../components/Header";
 import { NextLink } from "../../components/NextLink";
+import { supabase } from "../../api";
+
+type InputType = {
+  value: string,
+}
+
+interface FormEventElement {
+  email: InputType
+  password: InputType
+}
+
+type DivElementEvent = FormEventElement & HTMLDivElement;
 
 export default function SignUp() {
   const [isLoading, setLoading] = useState<boolean>();
+  const toast = useToast();
+  async function onSubmit(e: React.FormEvent<DivElementEvent>) {
+    e.preventDefault();
+    const defaultToastSettings = {
+      isClosable: true,
+    }
+    setLoading(true);
+    const{ user, session, error } = await supabase.auth.signUp({
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    })
+    if(error) {
+      toast({
+        ...defaultToastSettings,
+        title: `An error occured while creating your user`,
+        status: "error",
+      })
+
+    } else {
+      toast({
+        ...defaultToastSettings,
+        title: `User created`,
+        status: "success",
+      })
+    }
+    setLoading(false);
+  }
 
   return (
     <>
@@ -23,20 +64,18 @@ export default function SignUp() {
         <Heading as="h2" size="md">
           Create an account
         </Heading>
-        <Stack as="form" mt={4} w="xs">
-          <FormControl id="username">
-            <Input disabled={isLoading} placeholder="Username" />
-            <FormErrorMessage></FormErrorMessage>
-          </FormControl>
+        <Stack as="form" mt={4} w="xs" onSubmit={onSubmit}>
           <FormControl id="email">
+            <FormLabel>Email</FormLabel>
             <Input disabled={isLoading} placeholder="E-mail" />
             <FormErrorMessage></FormErrorMessage>
           </FormControl>
           <FormControl id="password">
-            <Input disabled={isLoading} placeholder="Password" />
+            <FormLabel>Password</FormLabel>
+            <Input disabled={isLoading} placeholder="Password" type="password" />
             <FormErrorMessage></FormErrorMessage>
           </FormControl>
-          <Button colorScheme="brand" isLoading={isLoading}>
+          <Button type="submit" colorScheme="brand" isLoading={isLoading}>
             Sign up
           </Button>
           <Box textAlign="center">
