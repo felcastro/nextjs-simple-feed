4abaab199d146073a6bb16950b6878dd3786497e
Felipe Castro <fpocastro@gmail.com>
2021-08-15 00:42:18 -0300

    Allowed selecting text on posts without navigating

--- components/LinkRoleBox/LinkRoleBox.tsx
+++ components/LinkRoleBox/LinkRoleBox.tsx
@@ -3,9 +3,15 @@ import { useRouter } from "next/router";
 
 export interface LinkRoleBoxProps extends BoxProps {
   href: string;
+  ignoreSelection?: boolean;
 }
 
-export const LinkRoleBox = ({ href, children, ...props }: LinkRoleBoxProps) => {
+export const LinkRoleBox = ({
+  href,
+  ignoreSelection = false,
+  children,
+  ...props
+}: LinkRoleBoxProps) => {
   const router = useRouter();
 
   return (
@@ -13,7 +19,12 @@ export const LinkRoleBox = ({ href, children, ...props }: LinkRoleBoxProps) => {
       role="link"
       tabIndex={0}
       cursor="pointer"
-      onClick={() => router.push(href)}
+      onClick={() => {
+        const selection = window.getSelection().toString();
+        if (ignoreSelection || selection.length <= 0) {
+          router.push(href);
+        }
+      }}
       {...props}
     >
       {children}
